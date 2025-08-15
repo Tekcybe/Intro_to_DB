26b956f788e6ddf4c5460f1bfe93819e1940dbfb
Peter Ojor <peter.ojor1@gmail.com>
2025-08-15 12:05:19 +0100

    Made changes to the MySQLServer.py file

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,10 +1,8 @@
-
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
-        # Connect to MySQL server (change user & password accordingly)
+        # Connect to MySQL server (update username/password)
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
@@ -15,12 +13,11 @@ def create_database():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-    
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+
+    except mysql.connector.Error as err:
+        print(f"Error while connecting to MySQL: {err}")
 
     finally:
-        # Close cursor & connection
         if 'cursor' in locals() and cursor is not None:
             cursor.close()
         if 'connection' in locals() and connection.is_connected():
